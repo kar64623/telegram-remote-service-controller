3061882881549fe6f5b7d8b621ffed215a80118e
kar <cdmx3099@amerike.edu.mx>
2025-11-29 15:03:32 -0600

    feat: Adding systemctl commands

--- systemActivator.go
+++ systemActivator.go
@@ -12,12 +12,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
+
 type osInfo struct {
 	hostname string 
 	date string 
 }
 
-var botToken string 
+var helpMessage = `
+Uso del bot: 
+	/start 	<servicio>
+	/status <servicio>
+	/stop 	<servicio>
+`
+
+
+var botToken 		 string 
 var stringChatId 	 string 
 
 func main() {
@@ -28,8 +38,10 @@ func main() {
 	botToken = os.Getenv("TOKEN_ID")
 	stringChatId = os.Getenv("CHAT_ID")
 	chatId := setString(stringChatId)
-	initBot(botToken, chatId)
+	info := setInfo()
+	initBot(botToken, chatId, info)
 }
+
 func setString(stringChatId string) int64 {
 	chatIdParsed, err := strconv.ParseInt(stringChatId, 10, 64)
 	if err != nil {
@@ -54,7 +66,7 @@ func getHostname() string {
 	return hostname
 }
 
-func initBot(botToken string, chatId int64) {
+func initBot(botToken string, chatId int64, info osInfo) {
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Println(err)
@@ -67,19 +79,81 @@ func initBot(botToken string, chatId int64) {
 		if update.Message.Chat.ID != chatId {
 			continue
 		}
-		if update.Message != nil {
+		if update.Message == nil {
 			continue
 		}
 		if !update.Message.IsCommand() {
 			continue
 		}
-	switch update.Message.Command() {
+	    service := update.Message.CommandArguments()
+	
+		if service == "" {
+			msg := tgbotapi.NewMessage(chatId, helpMessage)
+			_, err := bot.Send(msg)
+			if err != nil {
+				log.Println(err)
+			}
+			continue
+		} 
+
+	    var out string
+		switch update.Message.Command() {
 		case "start":
-			string := startService()
+			out = startService(service)
+		case "stop":
+			out = stopService(service)
+		case "status":
+			out = statusService(service)
+		default:
+			out = helpMessage
+		}
+		data := fmt.Sprintf("Respuesta de:\n%s\n%s\n%s", info.hostname, info.date, out)
+		msg := tgbotapi.NewMessage(chatId, data)
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Println(err)
 		}
 
 	}
 }
 
-func startService() {
+func startService(service string) string {
+	cmd := exec.Command("systemctl", "start", service)
+	out, err := cmd.CombinedOutput()
+	time.Sleep(10 * time.Second)
+	if err != nil {
+		log.Println(err)
+		return err.Error()
+	}
+	if string(out) ==  "" {
+		return "El servicio se inicio de manera correcta"
+	}
+	return string(out)
 }
+
+func stopService(service string) string {
+	cmd := exec.Command("systemctl", "stop", service)
+	out, err := cmd.CombinedOutput()
+	time.Sleep(10 * time.Second)
+	if err != nil {
+		log.Println(err)
+		return err.Error()
+	}
+	if string(out) == "" {
+		return "El servicio se desactivo de manera exitosa"
+	}
+	return string(out)
+
+}
+
+func statusService(service string) string {
+	cmd := exec.Command("systemctl", "status", service)
+	out, err := cmd.CombinedOutput()
+	time.Sleep(10 * time.Second)
+	if err != nil {
+		log.Println(err)
+		return err.Error()
+	}
+	return string(out)
+}
+
